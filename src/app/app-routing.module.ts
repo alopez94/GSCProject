4e2548e3e9dde5fd697c350f6a6ff2642f18dab6
alopez94 <allan.u94l@gmail.com>
2021-03-17 01:15:58 -0600

    Login/Landing start (not yet working)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContentLayoutComponent } from './layout/components/content-layout/content-layout.component';
+import { LoginComponent } from './auth/components/login/login.component';
+
 
 const routes: Routes = [
   {
     path: '',
-    component: ContentLayoutComponent,
+    component: LoginComponent,
   },
   {
     path: '**',
